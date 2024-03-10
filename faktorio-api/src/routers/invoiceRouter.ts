74b945321626ee7cfc9940f17beba060c588f6de
Jiří Špác <capajj@gmail.com>
2024-03-10 15:59:43 +0100

    Refactor invoiceRouter and NewInvoice component

--- faktorio-api/src/routers/invoiceRouter.ts
+++ faktorio-api/src/routers/invoiceRouter.ts
@@ -5,25 +5,25 @@ import { count, eq } from 'drizzle-orm'
 import { protectedProc } from '../isAuthorizedMiddleware'
 
 export const invoiceRouter = trpcContext.router({
-	create: protectedProc
-		.input(z.object({ title: z.string() }))
-		.mutation(({ input }) => {
-			return new Date()
-		}),
-	all: protectedProc.query(async ({ ctx }) => {
-		const invoicesForUser = await ctx.db.query.invoicesTb.findMany({
-			where: eq(invoicesTb.userId, ctx.userId)
-		})
+  create: protectedProc
+    .input(z.object({ title: z.string() }))
+    .mutation(({ input }) => {
+      return new Date()
+    }),
+  all: protectedProc.query(async ({ ctx }) => {
+    const invoicesForUser = await ctx.db.query.invoicesTb.findMany({
+      where: eq(invoicesTb.userId, ctx.userId)
+    })
 
-		return invoicesForUser
-	}),
-	count: protectedProc.query(async ({ ctx }) => {
-		const res = await ctx.db
-			.select({ value: count() })
-			.from(invoicesTb)
-			.where(eq(invoicesTb.userId, ctx.userId))
-			.execute()
+    return invoicesForUser
+  }),
+  count: protectedProc.query(async ({ ctx }) => {
+    const res = await ctx.db
+      .select({ count: count() })
+      .from(invoicesTb)
+      .where(eq(invoicesTb.userId, ctx.userId))
+      .execute()
 
-		return res
-	})
+    return res[0].count
+  })
 })
--- faktorio-fe/src/pages/NewInvoice.tsx
+++ faktorio-fe/src/pages/NewInvoice.tsx
@@ -2,31 +2,37 @@ import AutoForm from '@/components/ui/auto-form'
 import { invoiceInsertSchema } from '../../../faktorio-api/src/zodDbSchemas'
 import { z } from 'zod'
 import cc from 'currency-codes'
-
-const formSchema = invoiceInsertSchema
-	.pick({
-		number: true,
-		currency: true,
-		issued_on: true,
-		payment_method: true,
-		footer_note: true,
-		taxable_fulfillment_due: true
-	})
-	.extend({
-		// @ts-expect-error
-		currency: z.enum([...cc.codes()]).default('CZK'),
-		issued_on: z.date().default(new Date()),
-		payment_method: z
-			.enum(['bank', 'cash', 'card', 'cod', 'crypto', 'other'])
-			.default('bank'),
-		taxable_fulfillment_due: z.date()
-	})
+import { trpcClient } from '@/lib/trpcClient'
+import { djs } from '@/lib/djs'
 
 export const NewInvoice = () => {
-	return (
-		<div>
-			<h2>New Invoice</h2>
-			<AutoForm formSchema={formSchema}></AutoForm>
-		</div>
-	)
+  const [invoicesCount] = trpcClient.invoices.count.useSuspenseQuery()
+
+  const invoiceOrdinal = invoicesCount + 1
+  const nextInvoiceNumber = `${djs().format('YYYY')}-${invoiceOrdinal.toString().padStart(3, '0')}`
+  const formSchema = invoiceInsertSchema
+    .pick({
+      number: true,
+      currency: true,
+      issued_on: true,
+      payment_method: true,
+      footer_note: true,
+      taxable_fulfillment_due: true
+    })
+    .extend({
+      // @ts-expect-error
+      currency: z.enum([...cc.codes()]).default('CZK'),
+      issued_on: z.date().default(new Date()),
+      number: z.string().default(nextInvoiceNumber),
+      payment_method: z
+        .enum(['bank', 'cash', 'card', 'cod', 'crypto', 'other'])
+        .default('bank'),
+      taxable_fulfillment_due: z.date()
+    })
+  return (
+    <div>
+      <h2>New Invoice</h2>
+      <AutoForm formSchema={formSchema}></AutoForm>
+    </div>
+  )
 }
