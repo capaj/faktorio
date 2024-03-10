59976dae915d36577788490b8bc3865ffb7ae3a2
Jiří Špác <capajj@gmail.com>
2024-03-10 16:27:31 +0100

    fix deploy

--- faktorio-api/src/routers/contactCreateFormSchema.ts
+++ faktorio-api/src/routers/contactCreateFormSchema.ts
@@ -1,12 +1,13 @@
-import { z } from 'zod';
-import { contactInsertSchema } from '../zodDbSchemas';
+import { z } from 'zod'
+import { contactInsertSchema } from '../zodDbSchemas'
 
-
-export const contactCreateFormSchema = contactInsertSchema.omit({
-	id: true,
-	user_id: true,
-	created_at: true,
-	updated_at: true
-}).extend({
-	name: z.string()
-});
+export const contactCreateFormSchema = contactInsertSchema
+  .omit({
+    id: true,
+    user_id: true,
+    created_at: true,
+    updated_at: true
+  })
+  .extend({
+    name: z.string()
+  })
