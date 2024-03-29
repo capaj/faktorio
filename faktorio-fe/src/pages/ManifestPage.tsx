789890426c1b7ff53e9df0db4ccfb02e5ae462c1
Jiří Špác <capajj@gmail.com>
2024-03-29 16:26:32 +0100

    tweak manifest

--- faktorio-fe/src/pages/ManifestPage.tsx
+++ faktorio-fe/src/pages/ManifestPage.tsx
@@ -19,20 +19,30 @@ export const ManifestPage = () => {
         </p>
         <p className="mt-3">
           Daňové přiznání mě vždycky iritovalo. Musím se přiznat státu se všemi
-          svými příjmy, výdaji a když udělám chybu je zde nebezpečně vysoká
-          šance, že mi hrozí pokuta v nejhorším případě i vězení. Stát zároveň
-          daňovou legislativu neustále mění, takže musím být neustále ve střehu,
-          abych neudělal chybu. Stát by měl by měl být partnerem podnikatele.
-          Dát mu zdarma moderní a přívětivé nástroje pro vedení účetnictví a
-          daňové evidence. Bohužel tady selhává stát na plné čáře. Formuláře pro
-          daňové přiznání jsou extrémě složité a neintuitivní. Jsou to v
-          podstatě jen elektronické verze papírových formulářů.
+          svými příjmy, výdaji-budiž stát musí také z něčeho žít. Když udělám
+          chybu je zde nebezpečně vysoká šance, že mi hrozí pokuta v nejhorším
+          případě i vězení. Pokud vše vyplním správně dostanu nějaký pozitivní
+          feedback? Ale kdeže. Stát nedokáže ani říct "Děkuji, zvládli jste to
+          správně!" Když pak odešlete peníze na účet vaší krajské finanční
+          správy, tak máte stejný pocit, jako by jste ty peníze hodili do
+          kanálu.
+        </p>
+        <p>
+          Stát zároveň daňovou legislativu neustále mění, takže musím být
+          neustále ve střehu, abych neudělal chybu. Stát by měl by měl být
+          partnerem podnikatele. Dát mu zdarma moderní a přívětivé nástroje pro
+          vedení účetnictví a daňové evidence. Bohužel tady selhává stát na plné
+          čáře. Formuláře pro daňové přiznání jsou extrémě složité a
+          neintuitivní. Jsou to v podstatě jen elektronické verze papírových
+          formulářů.
         </p>
         <p className="mt-3">
           V ideálním světě by měl malý podnikatel jen vystavit faktury a celý
           zbytek daňové administrativy by měl být vyřešen automaticky. Toto je
           hlavním cílem faktorio.cz. Úplně automatizovat 99% daňové evidence.
-          Jako programátor si myslím, že máme šanci tento problém vyřešit.
+          Jako programátor si myslím, že máme šanci tento problém vyřešit. Už
+          jsem tomu pár hodin svého času obětoval a když vidím jak se to daří,
+          tak mě to motivuje k tomu pokračovat.
         </p>
         {/* TODO add donation links */}
       </main>
