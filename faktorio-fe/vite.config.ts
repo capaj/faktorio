bb48bac3175df004973c2fe63bebb8b04e70c718
Jiří Špác <capajj@gmail.com>
2024-04-07 14:54:52 +0200

    add ts ignore

--- faktorio-fe/vite.config.ts
+++ faktorio-fe/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+// @ts-ignore
 import { getGitHash } from '../src/getGitHash'
 
 const gitHash = getGitHash()
