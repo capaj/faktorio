c3dd383d214a23f729642b67c4e07b76f595bdf3
Jiri Spac <capajj@gmail.com>
2025-02-27 05:28:29 +0100

    introduce jwtUtils

--- faktorio-api/src/index.ts
+++ faktorio-api/src/index.ts
@@ -4,12 +4,12 @@ import { drizzle } from 'drizzle-orm/libsql'
 import { appRouter } from './trpcRouter'
 
 import { createClient } from '@libsql/client'
-import jwt, { type JwtPayload } from '@tsndr/cloudflare-worker-jwt'
 
 import * as schema from './schema'
 import colorize from '@pinojs/json-colorizer'
 import { TrpcContext } from './trpcContext'
-import { userT } from './schema'
+import { extractUserFromAuthHeader, generateToken } from './jwtUtils'
+
 export interface Env {
   TURSO_DATABASE_URL: string
   TURSO_AUTH_TOKEN: string
@@ -21,9 +21,6 @@ const corsHeaders = {
   'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
   'Access-Control-Max-Age': '86400'
 }
-
-// Token expiration time in seconds (1 year)
-const JWT_EXPIRATION = 60 * 60 * 24 * 365
 
 async function handleOptions(request: Request) {
   if (
@@ -64,39 +61,14 @@ export default {
 
     const createTrpcContext = async (): Promise<TrpcContext> => {
       const authHeader = request.headers.get('authorization')
+      const user = await extractUserFromAuthHeader(authHeader, env.JWT_SECRET)
 
-      let jwtPayload
-      if (authHeader) {
-        const token = authHeader.split(' ')[1]
-
-        try {
-          const isValid = await jwt.verify(token, env.JWT_SECRET)
-          if (isValid) {
-            jwtPayload = jwt.decode(token) as JwtPayload
-          }
-        } catch (error) {
-          console.error(error)
-        }
-      }
       return {
         db: drizzle(turso, { schema }),
         env,
-        user: jwtPayload?.payload?.user as typeof userT.$inferSelect,
+        user,
         req: request,
-        generateToken: async (user: typeof userT.$inferSelect) => {
-          const payload = {
-            ...user,
-            passwordHash: undefined,
-            googleId: undefined
-          }
-          return await jwt.sign(
-            {
-              user: payload,
-              exp: Math.floor(Date.now() / 1000) + JWT_EXPIRATION
-            },
-            env.JWT_SECRET
-          )
-        }
+        generateToken: (user) => generateToken(user, env.JWT_SECRET)
       }
     }
 
