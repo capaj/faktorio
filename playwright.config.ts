ef264de207912e5c29e2594ec0bbc82908db3a85
Jiri Spac <capajj@gmail.com>
2025-05-19 11:50:22 +0200

    longer time for timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ import { defineConfig, devices } from '@playwright/test'
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const isCI = !!process.env.CI
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -16,7 +17,7 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: isCI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
@@ -30,11 +31,11 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: !!process.env.CI,
+    headless: isCI,
     screenshot: 'only-on-failure',
     video: 'on-first-retry'
   },
-  timeout: 5000,
+  timeout: isCI ? 30000 : 5000,
   // You can optionally use a global setup file instead of fixtures approach
   // globalSetup: './e2e/global-setup.ts',
 
