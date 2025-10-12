9c33bf1a66e1276692b59ec0b0d8905179d6c4e6
Jiri Spac <capajj@gmail.com>
2025-10-12 10:51:14 +0200

    bump deps
    cleanup eslint deps

--- scripts/getGitHash.ts
+++ scripts/getGitHash.ts
@@ -5,7 +5,7 @@ export function getGitHash() {
   let gitSha: string | undefined
   try {
     gitSha = fs.readFileSync('git-commit-hash', 'utf8').trim() // on deployed docker image
-  } catch (err) {}
+  } catch {}
 
   if (gitSha) {
     return gitSha.substring(0, 8) // we cut to 8 chars because we don't care about hash collisions and whatnot
@@ -13,7 +13,7 @@ export function getGitHash() {
 
   try {
     gitSha = cp.execSync('git rev-parse HEAD').toString().trim() // on local dev machine
-  } catch (err) {}
+  } catch {}
 
   if (!gitSha) {
     console.warn('Could not get git commit hash') // TODO this happens on lambdas for now. Ideally we should copy git-commit-hash file inside the lambda package
