eef8bf8a66a7b571346d8108e80df82029cfcabe
Jiri Spac <capajj@gmail.com>
2026-02-05 17:36:03 +0100

    fix: enable editing of client contact on invoice edit page
    The second field (Odběratel/Customer) was hardcoded as disabled.
    This removes the disabled prop to allow users to change the client
    contact when editing an invoice.
    
    Closes the issue raised in Discord about the second field not being editable.

--- faktorio-fe/src/pages/invoice/EditInvoicePage.tsx
+++ faktorio-fe/src/pages/invoice/EditInvoicePage.tsx
@@ -320,7 +320,7 @@ export const EditInvoicePage = () => {
                 <FormItem className="flex flex-col flew-grow col-span-2">
                   <FormLabel>Odběratel</FormLabel>
                   <FormControl>
-                    <ContactComboBox {...field} disabled />
+                    <ContactComboBox {...field} />
                   </FormControl>
                 </FormItem>
               )}
